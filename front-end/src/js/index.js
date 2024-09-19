70010a440a757fc79c9ff0ac1535d760c9e1a6e8
Nikita Aswani <nikitaaswani@Nikitas-MacBook-Pro.local>
2024-09-19 21:26:02 +0530

    Complete New Relic Frontend Assesment.

--- front-end/src/js/index.js
+++ front-end/src/js/index.js
@@ -1,1 +1,31 @@
-console.log("Hello World");
+function addResponsiveClass() {
+  const buttons = document.querySelectorAll('button');
+
+  buttons.forEach(button => {
+    // Remove existing classes if any
+    button.classList.remove('mobile', 'tablet');
+
+    // Check the viewport width and add the appropriate class
+    if (window.innerWidth <= 768) {
+      button.classList.add('mobile');  // For mobile viewports (768px and below)
+    } else if (window.innerWidth <= 1024) {
+      button.classList.add('tablet');  // For tablet viewports (between 769px and 1024px)
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleMenu = document.getElementById('toggleMenu');
+  const collapsibleMenu = document.getElementById('collapsibleMenu');
+  const handleClick = () => {
+    collapsibleMenu.classList.toggle("hidden");
+  };
+
+  if (toggleMenu) {
+    toggleMenu.addEventListener('click', handleClick);
+  }
+
+  addResponsiveClass();
+});
+
+window.addEventListener('resize', addResponsiveClass);
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Open Sans"', 'sans-serif'],
+      },
+      fontSize: {
+        'h1': ['40px', '48px'],
+        'h2': ['32px', '48px'],
+        'h3': ['24px', '32px'],
+        'h4': ['20px', '28px'],
+        'body': ['16px', '28px'],
+      },
+      fontWeight: {
+        'h3': '600',
+        'h4': '600',
+        'link': '600',
+      },
+      colors: {
+        charcoal: '#2C2C2C',
+        teal: '#0AB0BF',
+        blue: '#0069CE',
+        darkTeal: '#00838F',
+        orange: '#FF8853',
+      },
+    },
   },
   plugins: [],
 }
